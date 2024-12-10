47db0abaafb974b2cf19dfaed7b3030f04152f21
Manuchar Chachanidze <manucharch@gmail.com>
2024-12-10 17:52:12 +0400

    workingon language toggle button placement

--- src/app/modules/home/components/home/home.component.ts
+++ src/app/modules/home/components/home/home.component.ts
@@ -1,8 +1,4 @@
-import { Component, OnDestroy, OnInit, signal } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-import { TranslateService } from '@ngx-translate/core';
-import { MainService } from '../../../../services/main.service';
-import { Subscription } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TitleService } from '../../../../services/title.service';
 
 @Component({
@@ -14,19 +10,7 @@ import { TitleService } from '../../../../services/title.service';
 export class HomeComponent implements OnInit, OnDestroy {
   constructor(private titleService: TitleService) {}
 
-  ngOnInit(): void {
-    // this.updateTitle();
-    // this.languageChangeSubscription =
-    //   this.translateService.onLangChange.subscribe(() => {
-    //     this.updateTitle();
-    //   });
-  }
+  ngOnInit(): void {}
 
   ngOnDestroy(): void {}
-
-  // private updateTitle(): void {
-  //   this.translateService.get('header.home').subscribe((title) => {
-  //     this.title.setTitle(title);
-  //   });
-  // }
 }
