04d447c4d3e16dac8ddf9a418976315271f88a88
Manuchar Chachanidze <manucharch@gmail.com>
2024-12-10 14:24:51 +0400

    created div with blaring border

---
+++ src/app/components/blured-border/blured-border.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blured-border',
+  imports: [],
+  templateUrl: './blured-border.component.html',
+  styleUrl: './blured-border.component.scss'
+})
+export class BluredBorderComponent {
+
+}
--- src/app/modules/main/components/main/main.component.ts
+++ src/app/modules/main/components/main/main.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../../../components/header/header.component';
 import { FooterComponent } from '../../../../components/footer/footer.component';
+import { BluredBorderComponent } from '../../../../components/blured-border/blured-border.component';
 
 @Component({
   selector: 'app-main',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    BluredBorderComponent,
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
