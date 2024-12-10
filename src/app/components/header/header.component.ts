17cad1cbe088a636581a11ffe24ed7cd83e0075d
Manuchar Chachanidze <manucharch@gmail.com>
2024-12-10 14:13:59 +0400

    hamburger DONE

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { LangToggleComponent } from '../lang-toggle/lang-toggle.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, TranslateModule, LangToggleComponent],
+  imports: [RouterLink, TranslateModule, CommonModule, LangToggleComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent implements OnInit {
-  ngOnInit(): void {}
+export class HeaderComponent {
+  isHiden: boolean = true;
+
+  toggle(): void {
+    this.isHiden = !this.isHiden;
+  }
 }
