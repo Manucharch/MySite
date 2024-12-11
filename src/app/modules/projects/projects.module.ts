1cc209575c129e820d283f608d3a7fb925f258a1
Manuchar Chachanidze <manucharch@gmail.com>
2024-12-11 09:28:11 +0400

    moving into section to component

--- src/app/modules/projects/projects.module.ts
+++ src/app/modules/projects/projects.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { RouterModule, Routes } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 const routes: Routes = [{ path: '', component: ProjectsComponent }];
 
 @NgModule({
   declarations: [ProjectsComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), TranslateModule],
 })
 export class ProjectsModule {}
